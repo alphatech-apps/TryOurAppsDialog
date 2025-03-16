0812e5abffc7783f67205720d985bdbab6230a3e
AlphaTech Apps <alphatech.apps.bd@gmail.com>
2025-03-16 10:45:57 +0600

    Initial commit

---
+++ sample/src/main/java/com/jakir/tryourappsby5timeappopeneveryday/DoRemoteJob.java
@@ -0,0 +1,24 @@
+package com.jakir.tryourappsby5timeappopeneveryday;
+
+import android.content.Context;
+
+import com.jakir.tryourappsdialog.TryOurAppHelper;
+
+
+//
+// Created by JAKIR HOSSAIN on 3/13/2025.
+//
+public class DoRemoteJob {
+    Context context;
+
+    public DoRemoteJob(Context context) {
+        this.context = context;
+
+        tryOurOtherAppsLoad();
+    }
+
+    public void tryOurOtherAppsLoad() {
+        String developerName = context.getString(R.string.developerName);
+        new TryOurAppHelper().tryOurOtherAppsLoad(context, developerName, 5); // openCountWant means how much time app open then try to load
+    }
+}
